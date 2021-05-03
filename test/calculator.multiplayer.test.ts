71264d15939e6cc1db4d8774ee6dc08b17be4283
Mark Freedman <mark.freedman@gmail.com>
2021-05-02 21:39:31 -0400

    cfg

--- test/calculator.multiplayer.test.ts
+++ test/calculator.multiplayer.test.ts
@@ -2,7 +2,7 @@
 // import * as tsUnit from 'tsunit.external';
 // import * as Calculations from './Scripts/Calculations';
 import Calculator from '../src/calculator';
-import { expect, TestSuite, Test } from 'testyts';
+import { expect, TestSuite, Test, TestCase } from 'testyts';
 
 @TestSuite()
 export class CalculatorMultiplierTest {
@@ -22,6 +22,17 @@ export class CalculatorMultiplierTest {
     @Test()
     public BadMultiply() {
         expect.toBeEqual(18, Calculator.Product(3, 5));
+        // this.areIdentical(15, Calculator.Multiply(5, 3));
+    }
+
+
+    
+    @Test()
+    @TestCase("1 x 3", 1, 3, 3)
+    @TestCase("4 x 5", 4, 5, 20)
+    @TestCase("18 x 2", 18, 2, 36)
+    public MultiCaseMultiplication(val1: number, val2: number, expectedProduct: number) {
+        expect.toBeEqual(expectedProduct, Calculator.Product(val1, val2));
         // this.areIdentical(15, Calculator.Multiply(5, 3));
     }
 }
